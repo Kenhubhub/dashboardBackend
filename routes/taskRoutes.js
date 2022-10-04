98748d13f717ec4cc45c8e3845f111caee1388e5
Kenhubhub <98609221+Kenhubhub@users.noreply.github.com>
2022-10-04 19:23:12 +0100

    imported correct controller

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const {getTasks,createTask,updateTask} = require("../controllers/taskController")
+const {getTasks,createTask,updateTask,deleteTask} = require("../controllers/taskController")
 //GET   retrieve all tasks for particular user
 router.get("/:id",getTasks)
 //POST to create a task - requires body param of userId,status,task
